da1f4061d737075a7f17a384a8c6cee330000855
Mirror83 <glenochieng045@gmail.com>
2023-08-28 19:00:52 +0300

    Added translucent overlay activated mobile menu

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 const navToggle = document.getElementById("nav-toggle");
 const navList = document.getElementById("nav-list");
-const sectionElements = document.getElementsByName("section");
+
+const overlay = document.getElementById("overlay");
 
 navToggle.addEventListener("click", () => {
     const isVisible = navList.getAttribute("data-visible");
     if (isVisible == "true") {
         navList.setAttribute("data-visible", "false");
+        overlay.setAttribute("data-visible", "false");
         navToggle.setAttribute("aria-expanded", "false");
-
-        // navToggle.textContent = "Menu";
+        // To re-enable scrolling after menu is removed
+        document.body.setAttribute("data-mobile-menu-visible", "false");
     } else {
         navList.setAttribute("data-visible", "true");
+        overlay.setAttribute("data-visible", "true");
         navToggle.setAttribute("aria-expanded", "true");
-        // navToggle.textContent = "Close";
+        // To disable scrolling after menu is visible
+        document.body.setAttribute("data-mobile-menu-visible", "true")
     }
-})
+});
+
+overlay.addEventListener("click", () => {
+    navList.setAttribute("data-visible", "false");
+    overlay.setAttribute("data-visible", "false");
+    navToggle.setAttribute("aria-expanded", "false");
+    // To re-enable scrolling after menu is removed
+    document.body.setAttribute("data-mobile-menu-visible", "false");
+});
